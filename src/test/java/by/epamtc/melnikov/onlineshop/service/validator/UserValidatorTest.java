6f175ee2b76eacf53d6876be91bd5b0262d95ba3
Vladislav Melnikov <notzloyhuman@gmail.com>
2022-01-25 22:48:54 +0300

    Update: Tests added

---
+++ src/test/java/by/epamtc/melnikov/onlineshop/service/validator/UserValidatorTest.java
@@ -0,0 +1,14 @@
+package by.epamtc.melnikov.onlineshop.service.validator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserValidatorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/by/epamtc/melnikov/onlineshop/util/HashGeneratorTest.java
@@ -0,0 +1,14 @@
+package by.epamtc.melnikov.onlineshop.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HashGeneratorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

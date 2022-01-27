74ce20083b4310c27b9925fc2d154ef4125d4976
Vladislav Melnikov <notzloyhuman@gmail.com>
2022-01-27 23:13:18 +0300

    Update: Localization filter added

---
+++ src/main/java/by/epamtc/melnikov/onlineshop/controller/filter/LocalizationFilter.java
@@ -0,0 +1,50 @@
+package by.epamtc.melnikov.onlineshop.controller.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ * Servlet Filter implementation class LocalizationFilter
+ */
+public class LocalizationFilter implements Filter {
+
+	private static final String DEFAULT_CHARSET_ENCODING = "UTF-8";
+	
+    /**
+     * Default constructor. 
+     */
+    public LocalizationFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}

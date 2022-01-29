36b7ff13b48bca1505b93bc80c3389edacb09ca0
Vladislav Melnikov <notzloyhuman@gmail.com>
2022-01-29 23:42:05 +0300

    Update: Controller layer updated. LocalizationFilter added

--- src/main/java/by/epamtc/melnikov/onlineshop/controller/filter/LocalizationFilter.java
+++ src/main/java/by/epamtc/melnikov/onlineshop/controller/filter/LocalizationFilter.java
@@ -1,50 +1,66 @@
 package by.epamtc.melnikov.onlineshop.controller.filter;
 
 import java.io.IOException;
+import java.util.Locale;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import by.epamtc.melnikov.onlineshop.controller.JSPAttributeStorage;
+import by.epamtc.melnikov.onlineshop.controller.LocaleStorage;
 
 /**
  * Servlet Filter implementation class LocalizationFilter
+ * 
+ * @author nearbyall
+ *
  */
 public class LocalizationFilter implements Filter {
 
 	private static final String DEFAULT_CHARSET_ENCODING = "UTF-8";
 	
-    /**
-     * Default constructor. 
-     */
-    public LocalizationFilter() {
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see Filter#destroy()
-	 */
-	public void destroy() {
-		// TODO Auto-generated method stub
+	@Override
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest) servletRequest;
+		HttpServletResponse response = (HttpServletResponse) servletResponse;
+		setLocalization(request, response);
+		request.setCharacterEncoding(DEFAULT_CHARSET_ENCODING);
+		filterChain.doFilter(servletRequest, servletResponse);
 	}
 
-	/**
-	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
-	 */
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		// TODO Auto-generated method stub
-		// place your code here
-
-		// pass the request along the filter chain
-		chain.doFilter(request, response);
+	private void setLocalization(HttpServletRequest request, HttpServletResponse response) {
+		Object langAttribute = request.getSession().getAttribute(JSPAttributeStorage.LANGUAGE_CURRENT_PAGE);
+		if (langAttribute == null) {
+			Cookie[] cookies = request.getCookies();
+			if (cookies == null) {
+				setLocaleToCookieAndSession(request, response);
+			} else {
+				String cookieLang = null;
+				for (Cookie cookie : cookies) {
+					if (cookie.getName().equals(JSPAttributeStorage.LANGUAGE_CURRENT_PAGE)) {
+						cookieLang = cookie.getValue();
+						request.getSession().setAttribute(JSPAttributeStorage.LANGUAGE_CURRENT_PAGE, cookieLang);
+					}
+				}
+				if (cookieLang == null) {
+					setLocaleToCookieAndSession(request, response);
+				}
+			}
+		}
 	}
 
-	/**
-	 * @see Filter#init(FilterConfig)
-	 */
-	public void init(FilterConfig fConfig) throws ServletException {
-		// TODO Auto-generated method stub
+	private void setLocaleToCookieAndSession(HttpServletRequest request, HttpServletResponse response) {
+		String currentLang = request.getLocale().getLanguage();
+		Locale resultLocale = LocaleStorage.getLocaleFromLanguage(currentLang).getLocale();
+		Cookie langCookie = new Cookie(JSPAttributeStorage.LANGUAGE_CURRENT_PAGE, resultLocale.getLanguage());
+		response.addCookie(langCookie);
+		request.getSession().setAttribute(JSPAttributeStorage.LANGUAGE_CURRENT_PAGE, currentLang);
 	}
 
 }
